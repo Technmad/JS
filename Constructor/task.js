a50cf0eeb4f063861b27e9d0263a42ea242b3bc1
anurag <anuragpathakoffl@gmail.com>
2023-08-01 20:44:20 +0530

    javaScriptInfo tasks

---
+++ Constructor/task.js
@@ -0,0 +1,60 @@
+let head = {
+  glasses: 1,
+};
+
+let table = {
+  pen: 3,
+  __proto__: head,
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2,
+  __proto__: table,
+};
+
+let pockets = {
+  money: 2000,
+  __proto__: bed,
+};
+
+//console.log(pockets.pen);
+
+let animal = {
+  eat() {
+    this.full = true;
+  },
+};
+
+let rabbit = {
+  __proto__: animal,
+};
+
+rabbit.eat();
+// console.log(rabbit.full);
+// console.log(animal.full);
+
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach.push(food);
+  },
+};
+
+let speedy = {
+  stomach: [],
+  __proto__: hamster,
+};
+
+let lazy = {
+  stomach: [],
+  __proto__: hamster,
+};
+
+// This one found the food
+speedy.eat("apple");
+console.log(speedy.stomach); // apple
+
+// This one also has it, why? fix please.
+console.log(lazy.stomach); // apple
