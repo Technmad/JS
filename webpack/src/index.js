6ea1291305bf2ce6969b54470005bc9110c3192e
anurag <anuragpathakoffl@gmail.com>
2023-08-12 21:18:15 +0530

    element creation

---
+++ webpack/src/index.js
@@ -0,0 +1,7 @@
+function comoponent() {
+  const element = document.createElement("div");
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  return element;
+}
+
+document.body.appendChild(comoponent());
