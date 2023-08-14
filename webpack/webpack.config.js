37a3cc7c82cc606d6f6ded2adc961037f182f1e7
anurag <anuragpathakoffl@gmail.com>
2023-08-15 00:17:14 +0530

    config HTMLwebPlugin

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
-
+const plugin = require("html-webpack-plugin");
 module.exports = {
   // entry: "./src/index.js",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+
+  plugins: [
+    new plugin({
+      title: "Output Management",
+    }),
+  ],
 
   output: {
     // filename: "main.js",
