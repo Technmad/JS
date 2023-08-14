d1a68cdd7058aded3201d0dc2fd98095693f2d4b
anurag <anuragpathakoffl@gmail.com>
2023-08-15 00:27:48 +0530

    added development mode

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,14 +2,16 @@ const path = require("path");
 const plugin = require("html-webpack-plugin");
 module.exports = {
   // entry: "./src/index.js",
+  mode: "development",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
 
+  devtool: "inline-source-map",
   plugins: [
     new plugin({
-      title: "Output Management",
+      title: "devlopment",
     }),
   ],
 
