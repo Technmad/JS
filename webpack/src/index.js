3faea642f4694f63a6cebaed92004edd7c8d34ab
anurag <anuragpathakoffl@gmail.com>
2023-08-14 22:14:39 +0530

    added css import, hello class

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,8 +1,10 @@
 import myName from "./myName";
+import "./style.css";
 
 function comoponent() {
   const element = document.createElement("div");
   element.textContent = myName("Anurag");
+  element.classList.add("hello");
   return element;
 }
 
