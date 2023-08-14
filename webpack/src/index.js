94d4732c13e9e49aea692a07ffa0120d0081fdc5
anurag <anuragpathakoffl@gmail.com>
2023-08-15 00:11:22 +0530

    added print btn

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,8 +1,16 @@
 import myName from "./myName";
+import printMe from "./print.js";
 
 function comoponent() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
+
   element.textContent = myName("Anurag");
+
+  btn.innerHTML = "click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
