5b1342a0b5fb4e950b759cd0d2e4dc7405d21f7e
anurag <anuragpathakoffl@gmail.com>
2023-08-14 23:45:29 +0530

    add config for toml, yaml, jason5

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const toml = require("toml");
+const yaml = require("yamljs");
+const json5 = require("json5");
 
 module.exports = {
   entry: "./src/index.js",
@@ -33,6 +36,30 @@ module.exports = {
       {
         test: /\.xml$/i,
         use: ["xml-loader"],
+      },
+      // toml
+      {
+        test: /\.toml$/i,
+        type: "json",
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      // yaml
+      {
+        test: /\.yaml$/i,
+        type: "json",
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      // jason5
+      {
+        test: /\.json5$/i,
+        type: "json",
+        parser: {
+          parse: json5.parse,
+        },
       },
     ],
   },
