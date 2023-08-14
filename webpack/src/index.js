566e24f45edb81e28f5d19e89a752fdb8b1ea8ae
anurag <anuragpathakoffl@gmail.com>
2023-08-14 22:36:23 +0530

    imported icon, added icon component

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,10 +1,17 @@
 import myName from "./myName";
 import "./style.css";
+import Icon from "./icon.png";
 
 function comoponent() {
   const element = document.createElement("div");
   element.textContent = myName("Anurag");
   element.classList.add("hello");
+
+  // adding image to the file
+  const myIcon = new Icon();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
   return element;
 }
 
