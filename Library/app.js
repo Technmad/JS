8c5324504604e25c96f17ec50c8dea32a366e090
anurag <anuragpathakoffl@gmail.com>
2023-08-17 14:38:51 +0530

    initial commit

---
+++ Library/app.js
@@ -0,0 +1,27 @@
+const Library = [];
+
+function Book(title, author, pages, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
+}
+
+function addBookToLibrary() {
+  const newBook = new Book(title, author, pages, read);
+  Library.push(newBook);
+  for (let i = 0; i < Library.length; ++i) {
+    displayBooks(Library[i]);
+  }
+}
+
+function displayBooks(items) {
+  console.log(items);
+}
+
+const newBookBtn = document.querySelector("#new-book-btn");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const readStatus = document.querySelector("#status");
+const submitBtn = document.querySelector("#submit-btn");
